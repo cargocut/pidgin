6b8c5a98ccf91b0bbe7f71860d094728e3e043fe
d-plaindoux <d.plaindoux@free.fr>
2026-06-24 08:41:19 +0200

    Refactoring

--- test/kind_construct_test.ml
+++ test/kind_construct_test.ml
@@ -2,49 +2,49 @@ open Pidgin
 
 let%expect_test "construct: null" =
   let t = Kind.Construct.nothing () in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| nothing |}]
 ;;
 
 let%expect_test "construct: opt int" =
   let t = Kind.Construct.opt (Kind.Construct.int ()) in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| int? |}]
 ;;
 
 let%expect_test "construct: boolean" =
   let t = Kind.Construct.bool () in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| bool |}]
 ;;
 
 let%expect_test "construct: int" =
   let t = Kind.Construct.int () in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| int |}]
 ;;
 
 let%expect_test "construct: float" =
   let t = Kind.Construct.float () in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| float |}]
 ;;
 
 let%expect_test "construct: string" =
   let t = Kind.Construct.string () in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| string |}]
 ;;
 
 let%expect_test "construct: empty list" =
   let t = Kind.Construct.list [] in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| [] |}]
 ;;
 
 let%expect_test "construct: list with one element" =
   let t = Kind.Construct.list [ Kind.Construct.nothing () ] in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| [nothing] |}]
 ;;
 
@@ -52,25 +52,25 @@ let%expect_test "construct: list with two elements" =
   let t =
     Kind.Construct.list [ Kind.Construct.nothing (); Kind.Construct.nothing () ]
   in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| [nothing,nothing] |}]
 ;;
 
 let%expect_test "construct: list_of with two integers" =
   let t = Kind.Construct.list_of Kind.Construct.int [ 0; 1 ] in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| [int,int] |}]
 ;;
 
 let%expect_test "construct: empty record" =
   let t = Kind.Construct.record [] in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| {} |}]
 ;;
 
 let%expect_test "construct: record with one element" =
   let t = Kind.Construct.record [ "f", Kind.Construct.nothing () ] in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| {"f":nothing} |}]
 ;;
 
@@ -79,7 +79,7 @@ let%expect_test "construct: record with two elements" =
     Kind.Construct.record
       [ "f1", Kind.Construct.nothing (); "f2", Kind.Construct.nothing () ]
   in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| {"f1":nothing,"f2":nothing} |}]
 ;;
 
@@ -89,7 +89,7 @@ let%expect_test "construct: record_of with two integers" =
       (fun (k, v) -> k, Kind.Construct.int v)
       [ "f1", 0; "f2", 1 ]
   in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| {"f1":int,"f2":int} |}]
 ;;
 
@@ -98,6 +98,6 @@ let%expect_test "construct: record_of with heterogeneous values" =
     Kind.Construct.record
       [ "f1", Kind.Construct.int 0; "f2", Kind.Construct.string "1" ]
   in
-  print_string (Render_kind.to_string t);
+  print_string (Kind_render.to_string t);
   [%expect {| {"f1":int,"f2":string} |}]
 ;;
--- test/repr_construct_test.ml
+++ test/repr_construct_test.ml
@@ -2,61 +2,61 @@ open Pidgin
 
 let%expect_test "construct: null" =
   let t = Repr.Construct.null () in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| null |}]
 ;;
 
 let%expect_test "construct: boolean true" =
   let t = Repr.Construct.bool true in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| true |}]
 ;;
 
 let%expect_test "construct: boolean false" =
   let t = Repr.Construct.bool false in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| false |}]
 ;;
 
 let%expect_test "construct: positive int" =
   let t = Repr.Construct.int 42 in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| 42 |}]
 ;;
 
 let%expect_test "construct: negative int" =
   let t = Repr.Construct.int (-42) in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| -42 |}]
 ;;
 
 let%expect_test "construct: positive float" =
   let t = Repr.Construct.float 42.24 in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| 42.24 |}]
 ;;
 
 let%expect_test "construct: empty list" =
   let t = Repr.Construct.list [] in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| [] |}]
 ;;
 
 let%expect_test "construct: empty string" =
   let t = Repr.Construct.string "" in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| "" |}]
 ;;
 
 let%expect_test "construct: not empty string" =
   let t = Repr.Construct.string "42" in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| "42" |}]
 ;;
 
 let%expect_test "construct: list with one element" =
   let t = Repr.Construct.list [ Repr.Construct.null () ] in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| [null] |}]
 ;;
 
@@ -64,25 +64,25 @@ let%expect_test "construct: list with two elements" =
   let t =
     Repr.Construct.list [ Repr.Construct.null (); Repr.Construct.null () ]
   in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| [null,null] |}]
 ;;
 
 let%expect_test "construct: list_of with two integers" =
   let t = Repr.Construct.list_of Repr.Construct.int [ 0; 1 ] in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| [0,1] |}]
 ;;
 
 let%expect_test "construct: empty record" =
   let t = Repr.Construct.record [] in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| {} |}]
 ;;
 
 let%expect_test "construct: record with one element" =
   let t = Repr.Construct.record [ "f", Repr.Construct.null () ] in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| {"f":null} |}]
 ;;
 
@@ -91,7 +91,7 @@ let%expect_test "construct: record with two elements" =
     Repr.Construct.record
       [ "f1", Repr.Construct.null (); "f2", Repr.Construct.null () ]
   in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| {"f1":null,"f2":null} |}]
 ;;
 
@@ -101,7 +101,7 @@ let%expect_test "construct: record_of with two integers" =
       (fun (k, v) -> k, Repr.Construct.int v)
       [ "f1", 0; "f2", 1 ]
   in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| {"f1":0,"f2":1} |}]
 ;;
 
@@ -110,6 +110,6 @@ let%expect_test "construct: record_of with heterogeneous values" =
     Repr.Construct.record
       [ "f1", Repr.Construct.int 0; "f2", Repr.Construct.string "1" ]
   in
-  print_string (Render_repr.to_string t);
+  print_string (Repr_render.to_string t);
   [%expect {| {"f1":0,"f2":"1"} |}]
 ;;
