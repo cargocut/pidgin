49521b505df9e235032b83b00162c9e18c6a17e7
mspwn <spawnmick@gmail.com>
2026-07-05 01:36:37 +0200

    Initialize `Pidgin_pp`

---
+++ lib/pp/pidgin_pp.mli
@@ -0,0 +1,27 @@
+(* Copyright (c) 2026, Cargocut and the Pidgin developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+(** A set of pretty-printers to handle the different types defined in
+    the Pidgin library. *)
+
+(** {1 Internal}
+
+    Pretty-printers, primarily for inspection, that describe Pidgin's
+    internal elements. *)
+
+(** Pretty-printer for {!type:Pidgin.Repr.t}. Uses a representation
+    similar to JSON. *)
+val repr : Format.formatter -> Pidgin.Repr.t -> unit
+
+(** Pretty-printer for {!type:Pidgin.Kind.t}. Try to make the Kinds
+    readable.. *)
+val kind : Format.formatter -> Pidgin.Kind.t -> unit
+
+(** {2 Errors}
+
+    Pretty-printers for printing errors. *)
+
+(** Pretty printer for Validation errors. *)
+(* val value_error : Format.formatter -> Pidgin.Check.value_error -> unit *)
