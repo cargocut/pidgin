1b715f26167f370573e848ce1dfff96834cfc61f
mspwn <spawnmick@gmail.com>
2026-07-05 01:01:44 +0200

    Add a toplevel module

---
+++ lib/pidgin.ml
@@ -0,0 +1,12 @@
+(* Copyright (c) 2026, Cargocut and the Pidgin developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+module Repr = Repr
+module Kind = Kind
+module Check = Check
+module Driver = Driver
+module Sexp = Sexp
+module Csexp = Csexp
+module Misc = Misc
